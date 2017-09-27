6efe91a9bd64661e83cb3075f265e722a8f85ea1
Akond Rahman <akond.rahman.buet@gmail.com>
2017-09-27 12:52:10 -0400

    launchpad  report mining

---
+++ utility.py
@@ -0,0 +1,9 @@
+import os
+
+
+
+def dumpContentIntoFile(strP, fileP):
+    fileToWrite = open( fileP, 'w')
+    fileToWrite.write(strP)
+    fileToWrite.close()
+    return str(os.stat(fileP).st_size)
