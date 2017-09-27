69188976dd751b66a43cac86dc3fc6c0cdaf3461
Akond Rahman <akond.rahman.buet@gmail.com>
2017-09-27 11:13:22 -0400

    bugzilla report mining

--- bugzilla_sec_kw.py
+++ bugzilla_sec_kw.py
@@ -9,7 +9,9 @@ import numpy as np
 import bugsy
 bugzilla_obj = bugsy.Bugsy(api_key="mWYEjiA4nOsii23LqFSuhotZyXJic5hRmMc5bFdm")
 bugzilla_file_name = 'moz.bug.ids.txt'
+sec_kw_file_name   = 'sec.kws.txt'
 not_exist_list = [10179904, 10131433, 1021152, 1021519, 1037104, 1041577, 1060410]
+
 counter = 0
 if __name__=='__main__':
    #bug_obj = bugzilla.Bugzilla(url="https://bugzilla.mozilla.org/rest/", api_key="mWYEjiA4nOsii23LqFSuhotZyXJic5hRmMc5bFdm")
@@ -18,6 +20,9 @@ if __name__=='__main__':
 
    with open(bugzilla_file_name) as f:
         content = f.readlines()
+   with open(sec_kw_file_name) as f_:
+        sec_kw_content = f_.readlines()
+   sec_kw_content = [lower(sec_kw) for sec_kw in sec_kw_content]
    bug_ids = [x.strip() for x in content]
    bug_ids = np.unique(bug_ids)
    bug_ids = [int(bug_id) for bug_id in bug_ids]
@@ -26,14 +31,18 @@ if __name__=='__main__':
    print 'before filtering:{}, after filtering:{}'.format(len(content), valid_bug_cnt)
    for bug_id in bug_ids:
        counter += 1
-       #the_bug = bugzilla_obj.get(427301)  #put numeric ID here
-       print bug_id
-       print '='*50
+       #print bug_id
+       #print '='*50
        try:
           the_bug = bugzilla_obj.get(bug_id)  #put numeric ID here
           comments = the_bug.get_comments()
           for comment_obj in comments:
-              print comment_obj.text
+              comment_msg =  comment_obj.text
+              comment_msg =  [lower(token_) for token_ in comment_msg]
+              common =  len(set(comment_msg).intersection(sec_kw_content))
+              if (common > 0):
+                 print 'BUGID:{},BUGMESSAGE:{}'.format(bug_id,comment_msg)
+                 print '*'*25
           print '='*50
           print "Processed {} bug IDs, {} left".format(counter, valid_bug_cnt - counter)
           print '='*50
