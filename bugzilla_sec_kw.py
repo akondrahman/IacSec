15537b4494bf3b1bbf2f9c9d1dee8eef386d7000
Akond Rahman <akond.rahman.buet@gmail.com>
2017-09-27 09:55:00 -0400

    bugzilla report mining

--- bugzilla_sec_kw.py
+++ bugzilla_sec_kw.py
@@ -4,13 +4,19 @@ Sep 27, 2017
 to get bugzilla reports' text
 and check for secuirty key words
 '''
-import bugzilla
-
-
+#import bugzilla
+import bugsy
+bugzilla_obj = bugsy.Bugsy(api_key="mWYEjiA4nOsii23LqFSuhotZyXJic5hRmMc5bFdm")
 if __name__=='__main__':
+   #bug_obj = bugzilla.Bugzilla(url="https://bugzilla.mozilla.org/rest/", api_key="mWYEjiA4nOsii23LqFSuhotZyXJic5hRmMc5bFdm")
+   #URL = "https://partner-bugzilla.redhat.com"
+   #bug_obj = bugzilla.Bugzilla(URL, api_key="mWYEjiA4nOsii23LqFSuhotZyXJic5hRmMc5bFdm")
 
-bug_obj = bugzilla.Bugzilla(url="https://bugzilla.mozilla.org/rest/", api_key="mWYEjiA4nOsii23LqFSuhotZyXJic5hRmMc5bFdm")
-the_bug = bug_obj.get_bug(1189311)  #put numeric ID here
-for bug_key, bug_vals in the_bug.iteritems(): 
-        print bug_key, bug_vals
-        print '='*50
+   the_bug = bugzilla_obj.get_bug(427301)  #put numeric ID here
+   bug_kws = the_bug[keywords]
+   print bug_kws
+   print '='*50
+   comments = the_bug.get_comments()
+   for comment_obj in comments():
+       print comment_obj.text
+   print '='*50
