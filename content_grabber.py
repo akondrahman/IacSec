3d5df69222e902929b946bb025821272049e9e19
Akond Rahman <akond.rahman.buet@gmail.com>
2017-11-08 10:21:08 -0500

    content grabber

--- content_grabber.py
+++ content_grabber.py
@@ -25,6 +25,10 @@ if __name__=='__main__':
     openstack_files = utility.getFileFromCSV(openstack_file)
     wikimedia_files = utility.getFileFromCSV(wikimedia_file)
 
+    bas_fcount, cis_fcount, mir_fcount, moz_fcount, ost_fcount, wik_fcount = len(bastion_files), len(cisco_files), len(mirantis_files), len(mozilla_files), len(openstack_files), len(wikimedia_files)
+    tot_fil_cnt = bas_fcount + cis_fcount + mir_fcount + moz_fcount + ost_fcount + wik_fcount
+    print 'Total files to analyze:', tot_fil_cnt
+    print '-'*100
     file_counter = 0
     list_of_ds = [bastion_files,  cisco_files, mirantis_files, mozilla_files , openstack_files, wikimedia_files]
     for ds_ in list_of_ds:
@@ -33,3 +37,4 @@ if __name__=='__main__':
             print '='*25 + str(file_counter) + 'START!' + '='*25
             print file_content
             print '='*25 + str(file_counter) + 'END!!!' + '='*25
+    print '-'*100
--- utility.py
+++ utility.py
@@ -4,9 +4,9 @@ Akond Rahman
 Nov 08, 2017
 Wednesday
 '''
-
+import csv
 import os
-
+import os.path
 
 
 def dumpContentIntoFile(strP, fileP):
@@ -23,7 +23,8 @@ def getFileFromCSV(the_ds_file):
       next(reader_, None)
       for row_ in reader_:
         full_path_of_file  = row_[1]  # the file name from dataset
-        with open(full_path_of_file, 'rU') as the_file:
+        if(os.path.exists(full_path_of_file)):
+           with open(full_path_of_file, 'rU') as the_file:
              content_full = the_file.read()
              all_file_content.append(content_full)   ### each file conent is now one big string
     return all_file_content
