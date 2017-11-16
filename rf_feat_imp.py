69869a969d1fa45cbad59ad114ff9d7bde89ff59
Akond Rahman <akond.rahman.buet@gmail.com>
2017-11-15 22:08:31 -0500

    feature selection using RF

--- rf_feat_imp.py
+++ rf_feat_imp.py
@@ -4,6 +4,8 @@ Nov 15, 2017
 Wednesday
 Feature importance for IaC Metrics using RF
 '''
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 def readDataset(fileParam, dataTypeFlag=True):
   if dataTypeFlag:
@@ -13,16 +15,20 @@ def readDataset(fileParam, dataTypeFlag=True):
   return data_set_to_return
 
 def calcFeatureImp(feature_vec, label_vec):
-    theRndForestModel = RandomForestClassifier()    
+    theRndForestModel = RandomForestClassifier()
 
 if __name__=='__main__':
-   full_ds, col_names=readDataset(ds_file_name)
-   full_rows, full_cols = np.shape(full_dataset_from_csv)
+   ds_file_name = '/Users/akond/Documents/AkondOneDrive/OneDrive/ProcessInIaC/dataset/OCT17_BASTION_FULL_PROCESS_DATASET.csv'
+
+
+
+   full_ds=readDataset(ds_file_name)
+   full_rows, full_cols = np.shape(full_ds)
    feature_cols = full_cols - 1
-   all_features = full_dataset_from_csv[:, 2:feature_cols]
-   all_labels  =  dataset_for_labels[:, feature_cols]
+   all_features = full_ds[:, 2:feature_cols]
+   all_labels  =  full_ds[:, feature_cols]
    defected_file_count     = len([x_ for x_ in all_labels if x_==1.0])
    non_defected_file_count = len([x_ for x_ in all_labels if x_==0.0])
    print "No of. defects={}, non-defects={}".format(defected_file_count, non_defected_file_count)
    print "-"*50
-   calcFeatureImp(full_ds)
+   #calcFeatureImp(full_ds)
