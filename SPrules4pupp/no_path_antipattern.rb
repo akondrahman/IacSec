46d0b402207eaf4d6a5cb22b84731687c2b57e16
Akond Rahman <akond.rahman.buet@gmail.com>
2017-11-24 16:14:21 -0500

    puppet-lint plugin: anti-patterns in path

--- SPrules4pupp/no_path_antipattern.rb
+++ SPrules4pupp/no_path_antipattern.rb
@@ -6,17 +6,13 @@ See if path values contain anti-patterns
 PuppetLint.new_check(:no_hardcode_key) do
 
   def check
-       #resource_indexes.each do |resource|
-           #resource[:param_tokens].each do |param_token|
-               #value_token   = param_token.next_code_token.next_code_token
-           tokens.each do |value_token|               
+           ### with resources we miss some path, so grab all tokens,a nd check them one-by-one
+           tokens.each do |value_token|
                token_val_str = value_token.value
                path_sym_cnt1  = token_val_str.count('/')
                path_sym_cnt2  = token_val_str.count('\\')
-               #print "#{path_sym_cnt} \n"
                ##first check if value is a directory or file, if valeu contains at least two slashes then proceed
                if path_sym_cnt1 > 1 or path_sym_cnt2 > 1
-                  #print "#{token_val_str} \n"
                   token_val_str=token_val_str.downcase
                   if token_val_str.include? "c:"
                      notify :warning, {
@@ -28,6 +24,5 @@ PuppetLint.new_check(:no_hardcode_key) do
                   end
                end
            end
-       #end
   end
 end
