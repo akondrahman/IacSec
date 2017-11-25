2bb12cc73ca7a8fc9b325cfcbe6b43f91fc314c8
Akond Rahman <akond.rahman.buet@gmail.com>
2017-11-25 17:34:06 -0500

    SPRulesForChef: add rules for Chef cookbooks

--- SPrules4chef/my_rules.rb
+++ SPrules4chef/my_rules.rb
@@ -5,6 +5,12 @@ Nov 25, 2017
 Saturday
 =end
 rule "MYRULE001", "SSH keys hould not be hard-coded" do
- tags %w{security akondrahman}
- 
+  tags %w{security akondrahman}
+  recipe do |ast_|
+     find_resources(ast_, :type => 'execute').select do |exec_reso|
+        #print "#{exec_reso}"
+        cmd_str = resource_attribute(exec_reso, 'command').to_s
+        print "#{cmd_str}"
+     end
+  end
 end
