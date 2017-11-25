eb7b24041adf1b4c0cb084d38824588d532f7fd9
Akond Rahman <akond.rahman.buet@gmail.com>
2017-11-24 19:02:50 -0500

    puppet-lint plugin: suspicious comments

--- SPrules4pupp/no_susp_comments.rb
+++ SPrules4pupp/no_susp_comments.rb
@@ -8,10 +8,17 @@ PuppetLint.new_check(:no_hardcode_key) do
 
   def check
      ##for comments we need to grab all lines
+     lineNo=0
      manifest_lines.each do |single_line|
         ##first check if string starts with #, which is comemnt in Puppet
-        if single_line.start_with?('#')
-           print "#{single_line}"
+        if single_line.include? '#'
+           lineNo += 1
+           ### check if those keywords exist
+           single_line=single_line.downcase
+           if (single_line.include?'bug' || single_line.include? 'hack' || single_line.include? 'fixme' || single_line.include? 'later' || single_line.include? 'later2' || single_line.include? 'todo')
+              print "#{single_line} #{lineNo}\n"
+              print "-----\n"
+           end
         end
      end
   end
