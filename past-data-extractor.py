6852053203eadbe83464628b4e5c1b43ce2409ae
Akond Rahman <akond.rahman.buet@gmail.com>
2018-02-07 09:54:47 -0500

    prep for past data

--- past-data-extractor.py
+++ past-data-extractor.py
@@ -4,6 +4,7 @@ Feb 07, 2018
 Script to extract time wise data extract
 '''
 import os
+import shutil
 
 def get_immediate_subdirs(a_dir):
     return [name for name in os.listdir(a_dir)
@@ -12,10 +13,18 @@ def get_immediate_subdirs(a_dir):
 def generatePastData(folder_path, y_p, m_p):
     all_dirs = get_immediate_subdirs(folder_path)
     for dir_ in all_dirs:
+        srcFolder = folder_path + dir_ + '/'
         for year_ in y_p:
             for mont_ in m_p:
-                folder2create = dir_ + '/' + year_ + '-' + mont_
+                folder2create = srcFolder + year_ + '-' + mont_ + '/'
+                print '='*50
                 print folder2create
+                if((os.path.exists(folder2create))==False):
+                try:
+                   shutil.copytree(srcFolder, folder2create)
+                except shutil.Error err_:
+                   print 'Directory not copied, error:', err_
+                print '='*50
 
 if __name__=='__main__':
    folder2walk = '/Users/akond/SECU_REPOS/test-pup/'
