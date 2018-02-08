b388e6d336818bf0b9d65af9a6e23bfe81dcc928
Akond Rahman <akond.rahman.buet@gmail.com>
2018-02-08 09:58:34 -0500

    slic: validity checks

---
+++ SLIC/constants.py
@@ -0,0 +1,8 @@
+'''
+Akond Rahman
+Feb 08, 2018
+This file holds all constants
+'''
+
+PP_EXT = '.pp'
+CH_EXT = '.rb'
--- SLIC/executor.py
+++ SLIC/executor.py
@@ -4,10 +4,21 @@ Feb 08, 2018
 Executor : detects the script type, and triggers the linter
 '''
 import os
+import constants
+
+def checkValidity(file_path):
+    # skip files that are in hidden directories, and in spec folders
+    flag2ret = False
+    if (('.' not in file_path) and ('spec' not in file_path)):
+        flag2flag2ret  = True
+    return flag2flag2ret
 
 def sniffSmells(path_to_dir):
-    for root, dirs, files in os.walk(path_to_dir):
-       for file_ in files:
-           if file_.endswith(".pp"):
-             print "Started analyzing:", os.path.join(root, file_)
+    for root_, dirs, files_ in os.walk(path_to_dir):
+       for file_ in files_:
+           if file_.endswith(constants.PP_EXT) or file_.endswith(constants.CH_EXT):
+             if checkValidity(file_):
+                print "Started analyzing:", os.path.join(root_, file_)
+             else:
+                print "Not analyzing, failed validity checks:", os.path.join(root_, file_)
              print "="*50
