a6a26f1842b35bb886ffaade80178bdba19a4637
Akond Rahman <akond.rahman.buet@gmail.com>
2018-02-08 10:08:41 -0500

    slic: validity checks

--- SLIC/constants.py
+++ SLIC/constants.py
@@ -6,3 +6,7 @@ This file holds all constants
 
 PP_EXT = '.pp'
 CH_EXT = '.rb'
+CH_DIR = 'recipe'
+TEST_DIR_SPEC = 'spec/'
+TEST_DIR_ACCE = 'acceptance/'
+DOT = '.'
--- SLIC/executor.py
+++ SLIC/executor.py
@@ -9,14 +9,14 @@ import constants
 def checkValidity(file_path):
     # skip files that are in hidden directories, and in spec folders
     flag2ret = False
-    if (('.' not in file_path) and ('spec' not in file_path)):
-        flag2flag2ret  = True
-    return flag2flag2ret
+    if ((file_path.count(constants.DOT) == 1) and (constants.TEST_DIR_SPEC not in file_path) and (constants.TEST_DIR_ACCE not in file_path)):
+        flag2ret  = True
+    return flag2ret
 
 def sniffSmells(path_to_dir):
     for root_, dirs, files_ in os.walk(path_to_dir):
        for file_ in files_:
-           if file_.endswith(constants.PP_EXT) or file_.endswith(constants.CH_EXT):
+           if file_.endswith(constants.PP_EXT) or (file_.endswith(constants.CH_EXT) and (constants.CH_DIR in file_)):
              if checkValidity(file_):
                 print "Started analyzing:", os.path.join(root_, file_)
              else:
