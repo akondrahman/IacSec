f4e1f925ce15bfcf75b3a9c8bee0adec7634be2c
Akond Rahman <akond.rahman.buet@gmail.com>
2018-02-10 00:31:15 -0500

    sampling code

--- sampling/sample_data_gen.py
+++ sampling/sample_data_gen.py
@@ -13,18 +13,35 @@ def checkValidity(file_path):
         flag2ret  = True
     return flag2ret
 
+def dumpContentIntoFile(strP, fileP):
+    fileToWrite = open( fileP, 'w')
+    fileToWrite.write(strP)
+    fileToWrite.close()
+    return str(os.stat(fileP).st_size)
+
 def generateData(path_to_dir):
+    str2ret = ''
     for root_, dirs_, files_ in os.walk(path_to_dir):
        for file_ in files_:
            if (file_.endswith('.pp') or file_.endswith('.rb')):
-                 tmp_month_str = root_.split('/')[4]
-                 month_str = tmp_month_str.split('-')[-2] + '-' + tmp_month_str.split('-')[-1]
-                 full_p_file = os.path.join(root_, file_)
-                 if (os.path.exists(full_p_file) and checkValidity(full_p_file) and (full_p_file.endswith('.rb')==False)):
-                    print 'Analyzing:', full_p_file, month_str
-                 elif (os.path.exists(full_p_file) and ('recipes' in full_p_file) and (full_p_file.endswith('.pp')==False)):
-                    print 'Analyzing:', full_p_file, month_str
+                 tmp_month_str = root_.split('/')[5]
+                 if '-' in tmp_month_str:
+                    month_str = tmp_month_str.split('-')[-2] + '-' + tmp_month_str.split('-')[-1]
+                    full_p_file = os.path.join(root_, file_)
+                    if (os.path.exists(full_p_file) and checkValidity(full_p_file) and (full_p_file.endswith('.rb')==False)):
+                       # print 'Analyzing:', full_p_file, month_str
+                       str2ret = str2ret + full_p_file + ',' + month_str + ',' + '\n'
+                    elif (os.path.exists(full_p_file) and ('recipes' in full_p_file) and (full_p_file.endswith('.pp')==False)):
+                       # print 'Analyzing:', full_p_file, month_str
+                       str2ret = str2ret + full_p_file + ',' + month_str + ',' + '\n'
+    return str2ret
+
 
 if __name__=='__main__':
-   ds_path = '/Users/akond/SECU_REPOS/test-pupp/'
-   generateData(ds_path)
+   ds_path  = '/Users/akond/SECU_REPOS/test-pupp/'
+   out_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/IacSec/sampling/test.csv'
+
+
+   str2Dump = generateData(ds_path)
+   out_byt  = dumpContentIntoFile(str2str2Dump, out_file)
+   print 'Dumped a file of {} bytes'.format(out_byt)
