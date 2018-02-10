cbdc2b1f0beb63e15e09df13d69e284834421b3a
Akond Rahman <akond.rahman.buet@gmail.com>
2018-02-10 15:59:25 -0500

    startfiied content: chef

--- chef_grabber.py
+++ chef_grabber.py
@@ -47,6 +47,25 @@ def getStarredRepos(file_):
     all_repo_list = [x_.split('/')[-1] for x_ in all_repo_list]
     return all_repo_list
 
+def getStratifiedChefScripts(ds_path_p):
+    cnt_file = 0
+    with open(ds_path_p, 'rU') as file_:
+      reader_ = csv.reader(file_)
+      next(reader_, None)
+      for row_ in reader_:
+          file2read = row_[0]
+          if(os.path.exists(file2read)):
+                cnt_file += 1
+                with open(file2read, 'rU') as the_file:
+                     content_full = the_file.read()
+                     print '='*25 + ':'*3 + str(valid_file)   + ':'*3  + 'START!' + '='*25
+                     print content_full
+                     print '*'*10
+                     print 'DECISION===>:'
+                     print '*'*10
+                     print '='*25 + ':'*3   + str(valid_file) + ':'*3  + 'END!!!' + '='*25
+    return cnt_file
+
 if __name__=='__main__':
     # root_dir = '/Users/akond/CHEF_REPOS/github-downloads/'
     # valid_repos=getValidRepos(root_dir + 'eligible_repos.csv')
@@ -69,9 +88,12 @@ if __name__=='__main__':
     # print '-'*100
     # getAllChefScripts(valid_repos, root_dir, repo_100stars)
 
-    root_dir = '/Users/akond/CHEF_REPOS/github-downloads/org_wise/'
-    valid_repos=getValidRepos(root_dir + 'final_valid_repo_names_v3.txt')
-    repo_100stars=getStarredRepos(root_dir + 'final_valid_repo_names_v3.txt') ### V3 inlcudes 3 orgs
-    print 'Total valid repos:', len(valid_repos)
-    print '-'*100
-    getAllChefScripts(valid_repos, root_dir, repo_100stars)
+    # root_dir = '/Users/akond/CHEF_REPOS/github-downloads/org_wise/'
+    # valid_repos=getValidRepos(root_dir + 'final_valid_repo_names_v3.txt')
+    # repo_100stars=getStarredRepos(root_dir + 'final_valid_repo_names_v3.txt') ### V3 inlcudes 3 orgs
+    # print 'Total valid repos:', len(valid_repos)
+    # print '-'*100
+    # getAllChefScripts(valid_repos, root_dir, repo_100stars)
+
+    cdat_stratified_ds = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/IacSec/sampling/STRATIFIED_CDAT.csv'
+    cdat_stra_fil_cnt = getStratifiedChefScripts(cdat_stratified_ds)
