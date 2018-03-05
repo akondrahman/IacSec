e0d99f07cecec849646ffe280735c8c7f6e39d33
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-05 11:15:31 -0500

    added pupp rule for http

---
+++ SPrules4pupp/no_http.rb
@@ -0,0 +1,21 @@
+=begin
+Akond Rahman
+write plugin for puppet-lint to check use of HTTP
+March 05, 2018
+=end
+PuppetLint.new_check(:no_http) do
+  def check
+           tokens.each do |indi_token|
+               token_valu = indi_token.value ### this gives each token
+               token_valu = token_valu.downcase
+               if token_valu.include? "http://"
+                  notify :warning, {
+                     message: 'SECURITY:::HTTP:::Do not use HTTP without TLS. This may cause a man in the middle attack. Use TLS with HTTP.',
+                     line:    indi_token.line,
+                     column:  indi_token.column,
+                     token:   token_valu
+                  }
+               end
+           end
+  end
+end
