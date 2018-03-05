e4774ff34fa823d30c8d5f8103d50c430d3282e1
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-05 12:46:13 -0500

    added puppet rule for empty password

--- SPrules4pupp/no_empty_pwd.rb
+++ SPrules4pupp/no_empty_pwd.rb
@@ -7,38 +7,34 @@ PuppetLint.new_check(:no_full_binding) do
   def check
            tokens.each do |indi_token|
                nxt_token     = indi_token.next_code_token # next token which is not a white space
-               if (nxt_token.nil?) || (indi_token.nil?)
-                  puts ""
-               else
+               if (!nxt_token.nil?) && (!indi_token.nil?)
                   token_type   = indi_token.type.to_s ### this gives type for current token
 
                   token_line   = indi_token.line ### this gives type for current token
                   nxt_tok_line = nxt_token.line
-                  nxt_nxt_token =  nxt_token.next_code_token # get the next next token
 
-                  if  (nxt_nxt_token.nil?)
-                     puts ""
-                  else
+                  nxt_nxt_token =  nxt_token.next_code_token # get the next next token to get key value pair
+
+                  if  (!nxt_nxt_token.nil?)
                      nxt_nxt_line = nxt_nxt_token.line
                      if (token_type.eql? 'NAME') || (token_type.eql? 'VARIABLE') || (token_type.eql? 'SSTRING')
-                        puts "Token type: #{token_type}"
+                        # puts "Token type: #{token_type}"
                         if (token_line==nxt_nxt_line)
                            token_valu  = indi_token.value.downcase
                            nxt_nxt_val = nxt_nxt_token.value.downcase
-                           puts "KEY,PAIR----->#{token_valu}, #{nxt_nxt_val}"
+                           # puts "KEY,PAIR----->#{token_valu}, #{nxt_nxt_val}"
+                           if(token_valu.include? "pwd") || (token_valu.include? "password") || (token_valu.include? "pass") && (nxt_nxt_val.length <=0)
+                              notify :warning, {
+                                message: 'SECURITY:::EMPTY_PASSWORD:::Do not keep password field empty. This may help an attacker to attack. Use hiera to import value.',
+                                line:    indi_token.line,
+                                column:  indi_token.column,
+                                token:   token_valu
+                              }
+                           end
                         end
                      end
                   end
-
                end
-               # if token_valu.include? "0.0.0.0"
-               #    notify :warning, {
-               #       message: 'SECURITY:::BINDING_TO_ALL:::Do not bind to 0.0.0.0. This may cause a DDOS attack. Restrict your available IPs.',
-               #       line:    indi_token.line,
-               #       column:  indi_token.column,
-               #       token:   token_valu
-               #    }
-               # end
            end
   end
 end
