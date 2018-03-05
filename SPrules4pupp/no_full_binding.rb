8ff0082300059b1349a2228939f1aaccaa6954c4
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-05 11:39:28 -0500

    added pupp rule for no binding to 0.0.0.0

---
+++ SPrules4pupp/no_full_binding.rb
@@ -0,0 +1,21 @@
+=begin
+Akond Rahman
+write plugin for puppet-lint to check binding to 0.0.0.0
+March 05, 2018
+=end
+PuppetLint.new_check(:no_full_binding) do
+  def check
+           tokens.each do |indi_token|
+               token_valu = indi_token.value ### this gives each token
+               token_valu = token_valu.downcase
+               if token_valu.include? "0.0.0.0"
+                  notify :warning, {
+                     message: 'SECURITY:::BINDING_TO_ALL:::Do not bind to 0.0.0.0. This may cause a DDOS attack. Restrict your available IPs.',
+                     line:    indi_token.line,
+                     column:  indi_token.column,
+                     token:   token_valu
+                  }
+               end
+           end
+  end
+end
