ac03fd1860b2928bc86ce5856ff38e17d6398559
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-05 10:53:43 -0500

    added puppet rule for base64

--- SPrules4pupp/no_base64.rb
+++ SPrules4pupp/no_base64.rb
@@ -1,18 +1,17 @@
 =begin
 Akond Rahman
-write plugin for puppet-lint to check use of MD5
+write plugin for puppet-lint to check use of BASE 64
 March 05, 2018
 =end
-PuppetLint.new_check(:no_md5) do
+PuppetLint.new_check(:no_base64) do
 
   def check
            tokens.each do |indi_token|
                token_valu = indi_token.value ### this gives each token
                token_valu = token_valu.downcase
-               # print "#{token_name} \t"
-               if token_valu.include? "md5"
+               if token_valu.include? "base64"
                   notify :warning, {
-                     message: 'SECURITY:::MD5:::Do not use MD5, as it has security weakness. Use SHA-512.',
+                     message: 'SECURITY:::BASE64:::Do not use BASE64 for security, as it is a message encoding scheme. Use SHA-512.',
                      line:    indi_token.line,
                      column:  indi_token.column,
                      token:   token_valu
