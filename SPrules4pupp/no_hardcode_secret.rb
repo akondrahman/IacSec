f0ebd975fd589f7a6ddeea8b0c4ad0c61bdbe437
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-05 19:48:17 -0500

    hard coded secret for all puppet scripts

--- SPrules4pupp/no_hardcode_secret.rb
+++ SPrules4pupp/no_hardcode_secret.rb
@@ -18,17 +18,18 @@ PuppetLint.new_check(:no_full_binding) do
                   if  (!nxt_nxt_token.nil?)
                      nxt_nxt_line = nxt_nxt_token.line
                      if (token_type.eql? 'NAME') || (token_type.eql? 'VARIABLE')
-                        puts "Token type: #{token_type}"
+                        # puts "Token type: #{token_type}"
                         if (token_line==nxt_nxt_line)
                            token_valu  = indi_token.value.downcase
                            nxt_nxt_val = nxt_nxt_token.value.downcase
-                           # puts "KEY,PAIR----->#{token_valu}, #{nxt_nxt_val}"
-                           if (((token_valu.include? "pwd")  || (token_valu.include? "password") || (token_valu.include? "pass") ||
+
+                           if (((token_valu.include? "pwd") || (token_valu.include? "password") || (token_valu.include? "pass") ||
                                 (token_valu.include? "uuid") || (token_valu.include? "key") || (token_valu.include? "crypt") ||
                                 (token_valu.include? "secret") || (token_valu.include? "certificate") || (token_valu.include? "id") ||
-                                (token_valu.include? "cert")   || (token_valu.include? "token") || (token_valu.include? "ssh_key") ||
-                                (token_valu.include? "md5")
+                                (token_valu.include? "cert") || (token_valu.include? "token") || (token_valu.include? "ssh_key") ||
+                                (token_valu.include? "md5") && (!token_valu.include? "{")
                                ) && ((nxt_nxt_val.length > 0)))
+                                 puts "KEY,PAIR,TYPE----->#{token_valu}, #{nxt_nxt_val}, #{token_type}"
                                  notify :warning, {
                                     message: 'SECURITY:::HARD_CODED_SECRET:::Do not hard code secrets. This may help an attacker to attack the system. You can use hiera to avoid this issue.',
                                     line:    indi_token.line,
