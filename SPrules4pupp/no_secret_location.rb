a7895c39bb80245b2a189e9a05a6f25bf514533d
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-06 10:10:28 -0500

    puppet code for exposing secret key location

---
+++ SPrules4pupp/no_secret_location.rb
@@ -0,0 +1,43 @@
+=begin
+Akond Rahman
+write plugin for puppet-lint to check exposure of secret key locations
+March 06, 2018
+=end
+PuppetLint.new_check(:no_expose_secret_location) do
+  def check
+           tokens.each do |indi_token|
+               nxt_token     = indi_token.next_code_token # next token which is not a white space
+               if (!nxt_token.nil?) && (!indi_token.nil?)
+                  token_type   = indi_token.type.to_s ### this gives type for current token
+
+                  token_line   = indi_token.line ### this gives type for current token
+                  nxt_tok_line = nxt_token.line
+
+                  nxt_nxt_token =  nxt_token.next_code_token # get the next next token to get key value pair
+
+                  if  (!nxt_nxt_token.nil?)
+                     nxt_nxt_line = nxt_nxt_token.line
+                     if (token_type.eql? 'NAME') || (token_type.eql? 'VARIABLE') || (token_type.eql? 'SSTRING')
+                        if (token_line==nxt_nxt_line)
+                           nxt_nxt_val = nxt_nxt_token.value.downcase
+                           # puts "KEY,PAIR----->#{token_valu}, #{nxt_nxt_val}"
+                           if ( ((nxt_nxt_val.include? "rsa")  || (nxt_nxt_val.include? "ssh") ||
+                                (nxt_nxt_val.include? "pem") || (nxt_nxt_val.include? "crt") ||
+                                (nxt_nxt_val.include? "key") || (nxt_nxt_val.include? "ssl") ||
+                                (nxt_nxt_val.include? "certificate") || (nxt_nxt_val.include? "crl")) &&
+                                (nxt_nxt_val.start_with? '/')
+                              )
+                              notify :warning, {
+                                message: 'SECURITY:::EXPOSING_SECRET_LOCATION:::Do not expose location of secrets. This may help an attacker to attack. You can use hiera to avoid this issue.',
+                                line:    indi_token.line,
+                                column:  indi_token.column,
+                                token:   token_valu
+                              }
+                           end
+                        end
+                     end
+                  end
+               end
+           end
+  end
+end
