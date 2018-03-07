f8bcb20bfc9a60a110610c535c2c832f56ad8351
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-07 13:20:27 -0500

    intgrating puppet rules with SLIC

--- SLIC/main.py
+++ SLIC/main.py
@@ -5,9 +5,18 @@ This code executes the lint engine
 '''
 import executor
 
+def giveTimeStamp():
+  tsObj = time.time()
+  strToret = datetime.datetime.fromtimestamp(tsObj).strftime('%Y-%m-%d %H:%M:%S')
+  return strToret
+
 if __name__=='__main__':
+   print 'Started at:', giveTimeStamp()
+   print '*'*125
    ds_path = '/Users/akond/SECU_REPOS/test-pupp/'
 
    # ds_path = '/Users/akond/SECU_REPOS/test-chef/'
 
    executor.sniffSmells(ds_path)
+   print '*'*125
+   print 'Ended at:', giveTimeStamp()
