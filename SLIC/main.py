b65ed81257a83b21f53bb5f746090410d2797936
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-07 18:33:52 -0500

    chef rules and SLIC integration

--- SLIC/main.py
+++ SLIC/main.py
@@ -8,6 +8,14 @@ import time
 import datetime
 import constants
 
+@contextmanager
+def getDuration():
+  t1 = time.time()
+  yield
+  t2 = time.time()
+  print("\n" + "-" * 72)
+  print("# Runtime: %.3f secs" % (t2-t1))
+
 def giveTimeStamp():
   tsObj = time.time()
   strToret = datetime.datetime.fromtimestamp(tsObj).strftime('%Y-%m-%d %H:%M:%S')
@@ -21,8 +29,11 @@ if __name__=='__main__':
    ds_path = '/Users/akond/SECU_REPOS/test-chef/'
 
    final_output_str = executor.sniffSmells(ds_path)
-   final_output_str = constants.HEADER_STR + '\n' +  final_output_str 
+   final_output_str = constants.HEADER_STR + '\n' +  final_output_str
    print '*'*100
    print final_output_str
    print '*'*100
    print 'Ended at:', giveTimeStamp()
+   print '*'*100
+   getDuration()
+   print '*'*100
