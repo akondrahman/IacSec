3ebc9c7599cd5fb5bf43131a55f5cd06ecc5743a
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-17 18:51:08 -0400

    analysis

--- analysis/frq_cnt.py
+++ analysis/frq_cnt.py
@@ -6,6 +6,7 @@ Mar 17, 2018
 import pandas as pd
 import numpy as np
 import datetime
+import matplotlib.pyplot as plt
 
 def calcSmellDensity(smell_cnt, file_list):
     val = 0
@@ -23,10 +24,21 @@ def sortDate(mon_lis):
     sorted_mon = [datetime.datetime.strftime(m_, "%Y-%m") for m_ in months]
     return sorted_mon
 
-def perfAnal(df_pa, header_pa):
+def makePlot(x_par, y_par, head_par, out_dir_par, type_par, ds_par):
+    plt.plot(x_par, y_par)
+    plt.title(head_par)
+    plt.xlabel('MONTH')
+    plt.ylabel(type_par)
+    #plt.show()
+    file2save = out_dir_par + head_par + '_' + type_par + '_' + ds_par + '.png'
+    plt.savefig(file2save)
+    plt.close()
+
+def perfAnal(df_pa, header_pa, output_dir):
     mon_lis = np.unique(df_pa['MONTH'].tolist())
     mon_lis = sortDate(mon_lis)
     for head_ in header_pa:
+        mon_plt_lis, cnt_plt_lis, sme_den_lis = [], [], []
         for mon_ in mon_lis:
             mon_df = df_pa[df_pa['MONTH']==mon_]
             per_mon_cnt = sum(mon_df[head_].tolist()) # we need the total count
@@ -35,15 +47,21 @@ def perfAnal(df_pa, header_pa):
             cnt_per_fil   = float(per_mon_cnt)/float(per_mon_fil_cnt)
             smell_density = calcSmellDensity(per_mon_cnt, per_mon_fil)
             print 'MON:{}, CNT:{}, FIL:{}, CNT_PER_FIL:{}, SMELL_DENS:{}, TYPE:{}'.format(mon_, per_mon_cnt, per_mon_fil_cnt, cnt_per_fil, smell_density, head_)
+            mon_plt_lis.append(mon_)
+            cnt_plt_lis.append(cnt_per_fil)
+            sme_den_lis.append(smell_density)
             print '*'*25
+        makePlot(mon_plt_lis, cnt_plt_lis, head_, output_dir, 'CNT_PER_FIL')
+        print '='*50
 
 if __name__=='__main__':
    results_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/V3_OUTPUT/V3_TEST_PUPPET.csv'
    results_df   = pd.read_csv(results_file)
+   plot_out_dir = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/plots_v3/'
    '''
    pass the needed colun headers
    '''
    needed_header = ['HARD_CODE_SECR',	'SUSP_COMM',	'SECR_LOCA',	'MD5_USAG',
                     'HTTP_USAG',	'BIND_USAG',	'EMPT_PASS',	'DFLT_ADMN',
                     'BASE_64',	'MISS_DFLT',	'TOTAL']
-   perfAnal(results_df, needed_header)
+   perfAnal(results_df, needed_header, plot_out_dir)
