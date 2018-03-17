149c61f8fa94bf16c01a5c00c95b970d0821b7af
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-17 18:20:54 -0400

    analysis

--- analysis/frq_cnt.py
+++ analysis/frq_cnt.py
@@ -6,16 +6,28 @@ Mar 17, 2018
 import pandas as pd
 import numpy as np
 
+
+def calcSmellDensity(smell_cnt, file_list):
+    val = 0
+    tot_lin = 0
+    for file_path in file_list:
+        file_ = open(file_path, 'rU')
+        num_lines = sum(1 for line_ in file_)
+        tot_lin = tot_lin + num_lines
+    val = float(smell_cnt)/float(tot_lin)
+    return val
+
 def perfAnal(df_pa, header_pa):
     mon_lis = np.unique(df_pa['MONTH'].tolist())
     for mon_ in mon_lis:
         for head_ in header_pa:
             mon_df = df_pa[df_pa['MONTH']==mon_]
             per_mon_cnt = sum(mon_df[head_].tolist()) # we need the total count
-            per_mon_fil = np.unique(mon_df['FILE_NAME'].tolist())
-            per_mon_fil_cnt = len(per_mon_fil)
-            cnt_per_fil = float(per_mon_cnt)/float(per_mon_fil_cnt)
-            print 'MON:{}, CNT:{}, FIL:{}, CNT_PER_FIL:{}, TYPE:{}'.format(mon_, per_mon_cnt, per_mon_fil_cnt, cnt_per_fil, head_)
+            per_mon_fil = np.unique(mon_df['FILE_NAME'].tolist()) # we need the unique file names
+            per_mon_fil_cnt = len(per_mon_fil) # unque file count
+            cnt_per_fil   = float(per_mon_cnt)/float(per_mon_fil_cnt)
+            smell_density = calcSmellDensity(per_mon_cnt, per_mon_fil)
+            print 'MON:{}, CNT:{}, FIL:{}, CNT_PER_FIL:{}, SMELL_DENS:{}, TYPE:{}'.format(mon_, per_mon_cnt, per_mon_fil_cnt, cnt_per_fil, smell_density, head_)
             print '*'*25
 
 if __name__=='__main__':
