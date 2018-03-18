0ec93e9a1f21ca7102dc96ff82128580a36ca1b6
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-18 13:18:47 -0400

    code for new and old checking

--- analysis/new_old_frq.py
+++ analysis/new_old_frq.py
@@ -29,24 +29,32 @@ def getSmellName(sme_nam):
             }
     return dict_[sme_nam]
 
+def processFileName(single_file_name):
+    splitted_dir_name = single_file_name.split('/')[5]
+    year  = splitted_dir_name.split('-')[-2]
+    month = splitted_dir_name.split('-')[-1]
+    str2del = '-' + year + '-' + month
+    str2ret = single_file_name.replace(str2del, '')
+    return str2ret
+
 def processPickle(pkl_p):
     # print pkl_p.head()
-
-    all_fil = np.unique(pkl_p['FILE_PATH'].tolist())
+    pkl_p['FLT_FIL'] = pkl_p['FILE_PATH'].apply(processFileName)
+    all_fil = np.unique(pkl_p['FLT_FIL'].tolist())
+    # print all_fil
     all_sme = np.unique(pkl_p['TYPE'].tolist())
 
     for file_name in all_fil:
         per_fil_df = pkl_p[pkl_p['FILE_PATH']==file_name]
-        for smel in all_sme:
-            per_fil_sme_df = per_fil_df[per_fil_df['TYPE']==smel]
-            # print per_fil_sme_df
-            per_fil_all_mon = np.unique(per_fil_sme_df['MONTH'].tolist())
-            per_fil_all_mon = [y_.replace(",", "") for y_ in per_fil_all_mon]
-            per_fil_all_mon = sortDate(per_fil_all_mon)
-            print per_fil_all_mon
-            for ind_ in xrange(len(per_fil_all_mon)):
-                fwd_ind = ind_ + 1
-                if (fwd_ind < len(per_fil_all_mon)):
+        print per_fil_df
+        per_fil_all_mon = np.unique(per_fil_df['MONTH'].tolist())
+        per_fil_all_mon = [y_.replace(",", "") for y_ in per_fil_all_mon]
+        per_fil_all_mon = sortDate(per_fil_all_mon)
+
+        # print per_fil_all_mon
+        for ind_ in xrange(len(per_fil_all_mon)):
+            fwd_ind = ind_ + 1
+            if (fwd_ind < len(per_fil_all_mon)):
                    curr_mon = per_fil_all_mon[ind_]
                    next_mon = per_fil_all_mon[fwd_ind]
 
@@ -54,7 +62,7 @@ def processPickle(pkl_p):
                    mon_next_df = per_fil_sme_df[next_mon]
                    curr_content_list = mon_curr_df['CONTENT'].tolist()
                    next_content_list = mon_next_df['CONTENT'].tolist()
-                   print 'current month:{}, next month:{}, current list:{}, next list:{}'.format(curr_mon, next_mon, curr_content_list, next_content_list)
+                   # print 'current month:{}, next month:{}, current list:{}, next list:{}'.format(curr_mon, next_mon, curr_content_list, next_content_list)
 
 if __name__=='__main__':
    orig_csv = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/V3_OUTPUT/V3_CDAT_CHEF.csv'
@@ -65,3 +73,8 @@ if __name__=='__main__':
    pkl_df  = pd.DataFrame([x for x in pkl_lis], columns=['MONTH', 'FILE_PATH', 'TYPE', 'CONTENT'])
 
    processPickle(pkl_df)
+
+        # for smel in all_sme:
+        #     per_fil_sme_df = per_fil_df[per_fil_df['TYPE']==smel]
+        #     print per_fil_sme_df
+        #     print '*'*25
