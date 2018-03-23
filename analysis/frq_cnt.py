622e59e163d0c5df970995d65c5b1ba30085b6a8
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-23 12:36:29 -0400

    unique file code

--- analysis/frq_cnt.py
+++ analysis/frq_cnt.py
@@ -50,7 +50,7 @@ def dumpContentIntoFile(strP, fileP):
 
 def makeCSV(lis_par, nam, dir):
     str_ = ''
-    header = 'MONTH,TYPE,CNT_PER_FIL,SMELL_DENSITY,UNI_FIL_CNT,'
+    header = 'MONTH,TYPE,CNT_PER_FIL,SMELL_DENSITY,UNI_FIL_PER,'
     for tup in lis_par:
         for elem in tup:
             str_ = str_ + str(elem) + ','
@@ -87,26 +87,32 @@ def perfAnal(df_pa, header_pa, output_dir, ds_name):
 
             per_mon_fil = mon_df['FILE_NAME'].tolist() # we need all file names, a smell can appear multiple times in a file
 
-            per_mon_fil_cnt = len(per_mon_fil) #  file count
+            per_mon_fil_cnt = len(np.unique(per_mon_fil)) #  file count
             cnt_per_fil   = round(float(per_mon_cnt)/float(per_mon_fil_cnt), 3)
             smell_density = calcSmellDensity(per_mon_cnt, per_mon_fil)
             # print 'MON:{}, CNT:{}, FIL:{}, CNT_PER_FIL:{}, SMELL_DENS:{}, TYPE:{}'.format(mon_, per_mon_cnt, per_mon_fil_cnt, cnt_per_fil, smell_density, head_)
             '''
             extra work for unique file: no of unique scripts for each at least one smell occur
             '''
-            all_fil_lis = mon_df['FILE_NAME'].tolist()
+            file_sme_df = mon_df[mon_df[head_] > 0]
+            all_fil_lis = file_sme_df['FILE_NAME'].tolist()
+            all_fil_lis = [processFileName(x_) for x_ in all_fil_lis]
+            # print all_fil_lis
             uni_fil_cnt = len(np.unique(all_fil_lis))
+            uni_fil_per = (float(uni_fil_cnt)/float(per_mon_fil_cnt))*100
+            # print uni_fil_cnt
+            # print '-'*10
 
             mon_plt_lis.append(mon_)
             cnt_plt_lis.append(cnt_per_fil)
             sme_den_lis.append(smell_density)
-            uni_fil_lis.append(uni_fil_cnt)
+            uni_fil_lis.append(uni_fil_per)
             # print '*'*25
-            csv_list.append((mon_, head_, cnt_per_fil, smell_density, uni_fil_cnt))
+            csv_list.append((mon_, head_, cnt_per_fil, smell_density, uni_fil_per))
 
         makePlot(mon_plt_lis, cnt_plt_lis, head_, output_dir, 'CNT_PER_FIL', ds_name)
         makePlot(mon_plt_lis, sme_den_lis, head_, output_dir, 'SMELL_DENSITY_KLOC', ds_name)
-        makePlot(mon_plt_lis, uni_fil_lis, head_, output_dir, 'UNI_FIL_CNT', ds_name)
+        makePlot(mon_plt_lis, uni_fil_lis, head_, output_dir, 'UNI_FIL_PER', ds_name)
         print '='*50
     makeCSV(csv_list, ds_name, output_dir)
 
