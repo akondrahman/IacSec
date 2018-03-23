fad8afbb562241dbf2cf14ec18ef93d5fc567a29
Akond Rahman <akond.rahman.buet@gmail.com>
2018-03-23 11:34:14 -0400

    overall summary code

--- analysis/frq_cnt.py
+++ analysis/frq_cnt.py
@@ -50,7 +50,7 @@ def dumpContentIntoFile(strP, fileP):
 
 def makeCSV(lis_par, nam, dir):
     str_ = ''
-    header = 'MONTH,TYPE,CNT_PER_FIL,SMELL_DENSITY,'
+    header = 'MONTH,TYPE,CNT_PER_FIL,SMELL_DENSITY,UNI_FIL_CNT,'
     for tup in lis_par:
         for elem in tup:
             str_ = str_ + str(elem) + ','
@@ -70,29 +70,34 @@ def perfAnal(df_pa, header_pa, output_dir, ds_name):
         for summary puprpose
         '''
         stat_list = df_pa[head_].tolist()
-        print 'SMELL:{},MIN:{},MEDIAN:{},MAX:{}'.format(head_, min(stat_list), np.median(stat_list), max(stat_list))
-        print '#'*50
-        mon_plt_lis, cnt_plt_lis, sme_den_lis = [], [], []
+        print 'DATASET:{},SMELL:{},MIN:{},MEDIAN:{},MAX:{}'.format(ds_nam, head_, min(stat_list), np.median(stat_list), max(stat_list))
+        uni_fil_lis, mon_plt_lis, cnt_plt_lis, sme_den_lis = [], [], [], []
         for mon_ in mon_lis:
             mon_df = df_pa[df_pa['MONTH']==mon_]
             per_mon_per_smell_list = mon_df[head_].tolist()
             per_mon_cnt = sum(per_mon_per_smell_list) # we need the total count
-            # per_mon_fil = np.unique(mon_df['FILE_NAME'].tolist()) # WRONG: unique file names exlcude the fact that a smell can appear mutliple tuime in the same file
 
             per_mon_fil = mon_df['FILE_NAME'].tolist() # we need all file names, a smell can appear multiple times in a file
 
             per_mon_fil_cnt = len(per_mon_fil) #  file count
             cnt_per_fil   = round(float(per_mon_cnt)/float(per_mon_fil_cnt), 3)
             smell_density = calcSmellDensity(per_mon_cnt, per_mon_fil)
-            print 'MON:{}, CNT:{}, FIL:{}, CNT_PER_FIL:{}, SMELL_DENS:{}, TYPE:{}'.format(mon_, per_mon_cnt, per_mon_fil_cnt, cnt_per_fil, smell_density, head_)
+            # print 'MON:{}, CNT:{}, FIL:{}, CNT_PER_FIL:{}, SMELL_DENS:{}, TYPE:{}'.format(mon_, per_mon_cnt, per_mon_fil_cnt, cnt_per_fil, smell_density, head_)
+            '''
+            extra work for unique file: no of unique scripts for each at least one smell occur
+            '''
+            uni_fil_cnt = len(np.unique(mon_df['FILE_NAME'].tolist()))
+
             mon_plt_lis.append(mon_)
             cnt_plt_lis.append(cnt_per_fil)
             sme_den_lis.append(smell_density)
-            print '*'*25
-            csv_list.append((mon_, head_, cnt_per_fil, smell_density))
+            uni_fil_lis.append(uni_fil_cnt)
+            # print '*'*25
+            csv_list.append((mon_, head_, cnt_per_fil, smell_density, uni_fil_cnt))
 
         makePlot(mon_plt_lis, cnt_plt_lis, head_, output_dir, 'CNT_PER_FIL', ds_name)
         makePlot(mon_plt_lis, sme_den_lis, head_, output_dir, 'SMELL_DENSITY_KLOC', ds_name)
+        makePlot(mon_plt_lis, uni_fil_lis, head_, output_dir, 'UNI_FIL_CNT', ds_name)
         print '='*50
     makeCSV(csv_list, ds_name, output_dir)
 
@@ -109,20 +114,24 @@ if __name__=='__main__':
    # plot_out_dir = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/plots_v3/'
    # ds_name = 'TEST'
 
-   results_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/V4_OUTPUT/V4_ALL_WIKIMEDIA_PUPPET.csv'
-   results_df   = pd.read_csv(results_file)
-   plot_out_dir = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/plots_v4_wik/'
-   ds_nam = 'WIKIMEDIA'
+   # results_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/V4_OUTPUT/V4_ALL_WIKIMEDIA_PUPPET.csv'
+   # results_df   = pd.read_csv(results_file)
+   # plot_out_dir = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/plots_v4_wik/'
+   # ds_nam = 'WIKIMEDIA'
 
    # results_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/V4_OUTPUT/V4_ALL_OPENSTACK_PUPPET.csv'
    # results_df   = pd.read_csv(results_file)
    # plot_out_dir = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/plots_v4_ost/'
    # ds_nam = 'OPENSTACK'
 
-   # results_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/V4_OUTPUT/V4_ALL_MOZILLA_PUPPET.csv'
-   # results_df   = pd.read_csv(results_file)
-   # plot_out_dir = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/plots_v4_moz/'
-   # ds_nam = 'MOZILLA'
+   results_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/V4_OUTPUT/V4_ALL_MOZILLA_PUPPET.csv'
+   results_df   = pd.read_csv(results_file)
+   plot_out_dir = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/plots_v4_moz/'
+   ds_nam = 'MOZILLA'
+
+   '''
+   CHEF REPOS
+   '''
 
    # results_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/V4_OUTPUT/V4_ALL_BERG_CHEF.csv'
    # results_df   = pd.read_csv(results_file)
@@ -138,7 +147,6 @@ if __name__=='__main__':
    # results_df   = pd.read_csv(results_file)
    # plot_out_dir = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/plots_v4_expr/'
    # ds_nam = 'EXPRESS42'
-
 
 
    perfAnal(results_df, needed_header, plot_out_dir, ds_nam)
