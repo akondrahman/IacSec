21a45f78ebdfff97c6778fa1b27884d898689a74
Akond Rahman <akond.rahman.buet@gmail.com>
2018-04-05 19:16:27 -0400

    Work for curated dataset

--- curated/file2categ.py
+++ curated/file2categ.py
@@ -6,6 +6,7 @@ This script maps each file to catgeory
 '''
 import pandas as pd
 import csv
+import numpy as np
 
 def getAPDict(file_):
     dict_ = {}
@@ -44,8 +45,30 @@ def getSubmDict(file_):
              if scriptID not in dict_:
                  dict_[scriptID] = [apID]
              else:
-                 dict_[scriptID] = [apID] + dict_[scriptID] 
+                 dict_[scriptID] = [apID] + dict_[scriptID]
     return dict_
+
+def getAgreement(dict_pa):
+    agrCnt, disAgrCnt  = 0, 0
+    for k_, v_ in dict_pa.iteritems():
+        scr_pat = getScriptPath(k_)
+        if (len(v_)==1):
+            agrCnt += 1
+            scr_lab = getLabel(v_)
+        elif (len(np.unique(v_))==1):
+            agrCnt += 1
+            scr_lab = getLabel(np.unique(v_)[0])
+        else:
+            scr_lab = str(np.bincount(v_).argmax())
+            scr_lab_occ = scr_lab.count(scr_lab)
+            tot_len = len(v_)
+            freq_perc  = float(scr_lab_occ)/float(tot_len)
+            # if the most frequent is 75% of the list , then agreement
+            if freq_perc >= 0.75:
+                agrCnt += 1
+            else:
+                disAgrCnt += 1 
+
 
 if __name__=='__main__':
    apa_tbl = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/datasets/curated/antipattern_table.csv'
@@ -72,4 +95,5 @@ if __name__=='__main__':
    get submissions
    '''
    sb_di = getSubmDict(sub_tbl)
-   print sb_di
+   # print sb_di
+   getAgreement(sb_di)
