a9158f5433e434a86504da046249064fb69ddc0c
Akond Rahman <akond.rahman.buet@gmail.com>
2018-04-05 18:37:39 -0400

    Work for curated dataset

--- curated/file2categ.py
+++ curated/file2categ.py
@@ -5,6 +5,19 @@ Thursday
 This script maps each file to catgeory
 '''
 import pandas as pd
+import csv
+
+def getDict(file_):
+    dict_ = {}
+    with open(file_, 'rU') as f:
+         reader_ = csv.reader(f)
+         next(reader_, None)
+         for row in reader_:
+             apID = row[0]
+             name = row[1]
+             if apID not in dict_:
+                 dict_[apID] = name
+    return dict_
 
 if __name__=='__main__':
    apa_tbl = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/datasets/curated/antipattern_table.csv'
@@ -15,18 +28,18 @@ if __name__=='__main__':
    '''
    get anti patterns
    '''
-   ap_df = pd.read_csv(apa_tbl)
-   # print ap_df.head()
+   ap_di = getDict(apa_tbl)
+   print ap_di
    '''
    get profile
    '''
    pr_df = pd.read_csv(pro_tbl)
-   # print pr_df.head()
+   print pr_df.head()
    '''
    get script table
    '''
    sc_df = pd.read_csv(scr_tbl)
-   # print sc_df.head()
+   print sc_df.head()
    '''
    get submissions
    '''
