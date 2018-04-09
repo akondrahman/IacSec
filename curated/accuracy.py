bfc92ff361b12346b4c71ee262b3c9b0d93ed380
Akond Rahman <akond.rahman.buet@gmail.com>
2018-04-09 15:02:30 +0200

    SLIC: accuracy calc

--- curated/accuracy.py
+++ curated/accuracy.py
@@ -9,8 +9,8 @@ from sklearn.metrics import precision_score, recall_score
 import numpy as np, pandas as pd
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 
-def printAccu(df2param):
-  # df2read = pd.read_csv(file_name)
+def printAccu(file_name):
+  df2read = pd.read_csv(file_name)
   actualLabels = df2param['ACTUAL'].tolist()
   predictedLabels = df2param['TOOL'].tolist()
   # print actualLabels
@@ -76,8 +76,10 @@ if __name__=='__main__':
    agree_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/curated/V6_CURATED_AGREE_PUPPET.csv'
    disagree_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/output/curated/V6_CURATED_DISAGREE_PUPPET.csv'
    curated_df = createDS(agree_file, disagree_file)
+   # need to dump csv for hacking, after dumping check CSV, make chnages if needed , and save it as final
+   curated_df.to_csv('/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/datasets/curated/V6_SEMIFINAL.csv')
    '''
    Step-2 : pass the dataset to get accuracy
    '''
-   # curated_ds = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/datasets/curated/Test.csv'
-   printAccu(curated_df)
+   # curated_ds = '/Users/akond/Documents/AkondOneDrive/OneDrive/SecurityInIaC/datasets/curated/FINAL.csv'
+   # printAccu(curated_df)
