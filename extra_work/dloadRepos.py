10a9cf2ce2a2b0bbae5c22d3312370f09c412e48
Akond Rahman <Akond.Ashfaque.Ur.Rahman@ibm.com>
2018-06-27 09:42:08 -0400

    mine more repos

--- extra_work/dloadRepos.py
+++ extra_work/dloadRepos.py
@@ -23,19 +23,32 @@ def getRepos(file_name):
 
 
 
-def grouper(the_list, size_, fillvalue=None):
-    args = [iter(the_list)] * size_
-    return izip_longest(*args, fillvalue=fillvalue)
+def makeChunks(the_list, size_):
+    for i in range(0, len(the_list), size_):
+        yield the_list[i:i+size_]
 
+def cloneRepo(repo_name):
+    cmd_ = "git clone " + repo_name
+    subprocess.check_output(['bash','-c', cmd_])    
+
+def cloneRepos(repo_list):
+    for repo_batch in repo_list:
+        str_ = ''
+        for repo_ in repo_batch:
+            print 'Cloning ', repo_
+            cloneRepo(repo_)
+            dirName = repo_.split('/')[-1].split('.')[0]
+            print dirName
 
 if __name__=='__main__':
    srcFile1='/Users/akond.rahman/Documents/Personal/misc/icse19-work/gh-repo-list-batch1.csv'
    srcFile2='/Users/akond.rahman/Documents/Personal/misc/icse19-work/gh-repo-list-batch2.csv'
    list1=getRepos(srcFile1)    
    list2=getRepos(srcFile2)      
-   list = list1 + list2 
-   list_ = np.unique(list)
+   list_ = list1 + list2 
+   list_ = np.unique(list_)
    print 'Repos to download:', len(list_)
    ## need to create chunks as too many repos 
-   chunked_list = grouper(list_, 1000)
-   print chunked_list
+   chunked_list = list(makeChunks(list_, 1000))  ### list of lists, at each batch download 1000 repos 
+   #print chunked_list
+   cloneRepos(chunked_list)
