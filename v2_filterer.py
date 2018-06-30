0f14d43f8964a77356f75d7c654388411e5c29d4
Akond Rahman <Akond.Ashfaque.Ur.Rahman@ibm.com>
2018-06-30 18:37:07 -0400

    filter github repos

--- v2_filterer.py
+++ v2_filterer.py
@@ -6,6 +6,7 @@ Get commit count and month diffs
 import os 
 import csv 
 from datetime import date
+import shutil 
 
 def dumpContentIntoFile(strP, fileP):
     fileToWrite = open( fileP, 'w')
@@ -22,7 +23,7 @@ def getMonDiff(st_mo, en_mo):
         mon_ = 1.0
     return mon_
 
-def findValidRepos(file_p):
+def findValidRepos(file_p, src_dir_p, des_dir_p):
     valid_list = []
     with open(file_p, 'rU') as file_:
         reader_ = csv.reader(file_)
@@ -34,18 +35,26 @@ def findValidRepos(file_p):
 
             months = getMonDiff(start_month, end_month)                        
             comm_per_mon = float(commit_count)/float(months)
-            print repo_name, months 
+            # print repo_name, months 
             if comm_per_mon >= 2.0  :
                 valid_list.append(repo_name)
     str_ = ''
     for repo_ in valid_list:
         str_ = str_ + repo_ + ',' + '\n'
-        
+        full_src_path = src_dir_p + repo_ + '/'
+        full_des_path = des_dir_p + repo_ + '/'
+        print 'SRC:' + full_src_path + ',DES:' + full_des_path
+        try:
+            shutil.copytree(full_src_path, full_des_path)
+        except shutil.Error as err_:
+            print 'Directory not copied, error:', err_
     return str_
                 
 
 if __name__=='__main__':
    the_file = '/Users/akond.rahman/Documents/Personal/IacSec/extra_work/v2_filtered_at_least_two.csv'
    out_fil = '/Users/akond.rahman/Documents/Personal/IacSec/extra_work/v3_filtered_list.csv' 
-   str_dump = findValidRepos(the_file)
+   src_dir = '/Users/akond.rahman/Documents/Personal/IacSec/extra_work/'
+   des_dir = '/Users/akond.rahman/Documents/Personal/IacSec/extra_work/final_repos/'
+   str_dump = findValidRepos(the_file, src_dir, des_dir)
    dumpContentIntoFile(str_dump, out_fil)
