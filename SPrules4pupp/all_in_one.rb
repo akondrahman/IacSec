010d0b697666583e8ce613b5a5988fed7645ac2a
Akond Rahman <akond.rahman.buet@gmail.com>
2018-07-07 11:47:03 -0400

    fixing slic: more restricition on user names and keys

--- SPrules4pupp/all_in_one.rb
+++ SPrules4pupp/all_in_one.rb
@@ -82,7 +82,7 @@ PuppetLint.new_check(:no_empty_pass) do
                            token_valu  = indi_token.value.downcase
                            nxt_nxt_val = nxt_nxt_token.value.downcase
                            # puts "KEY,PAIR----->#{token_valu}, #{nxt_nxt_val}"
-                           if (((token_valu.include? "pwd") || (token_valu.include? "password") || (token_valu.include? "pass")) && ((nxt_nxt_val.length <=0) || (nxt_nxt_val.eql?' ')))
+                           if (((token_valu.include? "pwd") || (token_valu.include? "password") || (token_valu.include? "pass")) && (((nxt_nxt_val.length <=0) || (nxt_nxt_val.eql?' ')) && (! nxt_nxt_val.include? "$")))
                               notify :warning, {
                                 message: 'SECURITY:::EMPTY_PASSWORD:::Do not keep password field empty. This may help an attacker to attack. You can use hiera to avoid this issue.@'+token_valu+'='+nxt_nxt_val+'@',
                                 line:    indi_token.line,
@@ -124,7 +124,7 @@ PuppetLint.new_check(:no_hardcode_secret_v1) do
   def check
            invalid_kw_list = ['(', 'undef', 'true', 'false', 'hiera', 'secret', 'union',
                               '$', '{', 'regsubst', 'hiera_hash', 'pick', 'get_ssl_property',
-                             'inline_template', 'under', 'mysql_password', '/']
+                             'inline_template', 'under', 'mysql_password', '/', 'ssl_ciphersuite', '::']
            tokens.each do |indi_token|
                nxt_token     = indi_token.next_code_token # next token which is not a white space
                if (!nxt_token.nil?) && (!indi_token.nil?)
@@ -144,16 +144,17 @@ PuppetLint.new_check(:no_hardcode_secret_v1) do
                            nxt_nxt_val  = nxt_nxt_token.value.downcase
                            nxt_nxt_type = nxt_nxt_token.type.to_s  ## to handle false positives,
                            # puts "KEY,PAIR----->#{token_valu}, #{nxt_nxt_val}"
-                           if (((token_valu.include? "pwd") || (token_valu.include? "password") || (token_valu.include? "pass") ||
-                                (token_valu.include? "uuid") || (token_valu.include? "key") || (token_valu.include? "crypt") ||
-                                (token_valu.include? "secret") || (token_valu.include? "certificate") || (token_valu.include? "id") ||
+                           # removed thiese: (token_valu.include? "id") and (token_valu.include? "uuid") ||
+                           if ((((token_valu.include? "pwd") || (token_valu.include? "password") || (token_valu.include? "pass") ||
+                                (token_valu.include? "key") || (token_valu.include? "crypt") ||
+                                (token_valu.include? "secret") || (token_valu.include? "certificate") ||
                                 (token_valu.include? "cert") || (token_valu.include? "token") || (token_valu.include? "ssh_key") ||
                                 (token_valu.include? "md5") || (token_valu.include? "rsa") || (token_valu.include? "ssl") ||
-                                (token_valu.include? "dsa") || (token_valu.include? "user")
+                                (token_valu.include? "dsa") || (token_valu.include? "user")) && (! token_valu.include? "::")
                                ) && ((! token_valu.include? "provider") && (!nxt_nxt_type.eql? 'VARIABLE') && (!invalid_kw_list.include? nxt_nxt_val) && (nxt_nxt_val.length > 1) )
                               )
                                  # && (nxt_nxt_val.is_a? String)
-                                 # puts "KEY,PAIR,CURR_TYPE,NEXT_TYPE----->#{token_valu}, #{nxt_nxt_val}, #{token_type}, #{nxt_nxt_type}"
+                                 #puts "KEY,PAIR,CURR_TYPE,NEXT_TYPE----->#{token_valu}, #{nxt_nxt_val}, #{token_type}, #{nxt_nxt_type}"
                                  notify :warning, {
                                     message: 'SECURITY:::HARD_CODED_SECRET_V1:::Do not hard code secrets. This may help an attacker to attack the system. You can use hiera to avoid this issue.@'+token_valu+'='+nxt_nxt_val+'@',
                                     line:    indi_token.line,
@@ -322,13 +323,13 @@ PuppetLint.new_check(:no_susp_comments) do
   end
 end
 
-####### after CCS reubuttal 
+####### after CCS reubuttal
 
 PuppetLint.new_check(:no_hardcode_secret_uname) do
   def check
            invalid_kw_list = ['(', 'undef', 'true', 'false', 'hiera', 'secret', 'union',
                               '$', '{', 'regsubst', 'hiera_hash', 'pick', 'get_ssl_property',
-                             'inline_template', 'under', 'mysql_password', '/']
+                             'inline_template', 'under', 'mysql_password', '/', 'ssl_ciphersuite', '::']
            tokens.each do |indi_token|
                nxt_token     = indi_token.next_code_token # next token which is not a white space
                if (!nxt_token.nil?) && (!indi_token.nil?)
@@ -338,6 +339,7 @@ PuppetLint.new_check(:no_hardcode_secret_uname) do
                   nxt_tok_line = nxt_token.line
 
                   nxt_nxt_token =  nxt_token.next_code_token # get the next next token to get key value pair
+                  # removed these: (token_valu.include? "uuid") || (token_valu.include? "id") ||
 
                   if  (!nxt_nxt_token.nil?)
                      nxt_nxt_line = nxt_nxt_token.line
@@ -346,7 +348,7 @@ PuppetLint.new_check(:no_hardcode_secret_uname) do
                            token_valu   = indi_token.value.downcase
                            nxt_nxt_val  = nxt_nxt_token.value.downcase
                            nxt_nxt_type = nxt_nxt_token.type.to_s  ## to handle false positives,
-                           if (( (token_valu.include? "uuid") || (token_valu.include? "id") || (token_valu.include? "user")
+                           if (( (token_valu.include? "user") && (! token_valu.include? "::")
                                ) && ((! token_valu.include? "provider") && (!nxt_nxt_type.eql? 'VARIABLE') && (!invalid_kw_list.include? nxt_nxt_val) && (nxt_nxt_val.length > 1) )
                               )
                                  notify :warning, {
@@ -366,19 +368,19 @@ end
 
 PuppetLint.new_check(:no_hardcode_secret_password) do
     def check
-             invalid_kw_list = ['(', 'undef', 'true', 'false', 'hiera', 'secret', 'union',
-                                '$', '{', 'regsubst', 'hiera_hash', 'pick', 'get_ssl_property',
-                               'inline_template', 'under', 'mysql_password', '/']
+           invalid_kw_list = ['(', 'undef', 'true', 'false', 'hiera', 'secret', 'union',
+                              '$', '{', 'regsubst', 'hiera_hash', 'pick', 'get_ssl_property',
+                             'inline_template', 'under', 'mysql_password', '/', 'ssl_ciphersuite', '::']
              tokens.each do |indi_token|
                  nxt_token     = indi_token.next_code_token # next token which is not a white space
                  if (!nxt_token.nil?) && (!indi_token.nil?)
                     token_type   = indi_token.type.to_s ### this gives type for current token
-  
+
                     token_line   = indi_token.line ### this gives type for current token
                     nxt_tok_line = nxt_token.line
-  
+
                     nxt_nxt_token =  nxt_token.next_code_token # get the next next token to get key value pair
-  
+
                     if  (!nxt_nxt_token.nil?)
                        nxt_nxt_line = nxt_nxt_token.line
                        if (token_type.eql? 'NAME') || (token_type.eql? 'VARIABLE')
@@ -389,6 +391,8 @@ PuppetLint.new_check(:no_hardcode_secret_password) do
                              if (((token_valu.include? "pwd") || (token_valu.include? "password") || (token_valu.include? "pass")
                                  ) && ((! token_valu.include? "provider") && (!nxt_nxt_type.eql? 'VARIABLE') && (!invalid_kw_list.include? nxt_nxt_val) && (nxt_nxt_val.length > 1) )
                                 )
+                                   print nxt_token.value.downcase
+                                   print ' '
                                    notify :warning, {
                                       message: 'SECURITY:::HARD_CODED_SECRET_PASSWORD:::Do not hard code secrets. This may help an attacker to attack the system. You can use hiera to avoid this issue.@'+token_valu+'='+nxt_nxt_val+'@',
                                       line:    indi_token.line,
@@ -407,19 +411,19 @@ PuppetLint.new_check(:no_hardcode_secret_password) do
 
 PuppetLint.new_check(:no_hardcode_secret_key) do
      def check
-              invalid_kw_list = ['(', 'undef', 'true', 'false', 'hiera', 'secret', 'union',
-                                 '$', '{', 'regsubst', 'hiera_hash', 'pick', 'get_ssl_property',
-                                'inline_template', 'under', 'mysql_password', '/']
+           invalid_kw_list = ['(', 'undef', 'true', 'false', 'hiera', 'secret', 'union',
+                              '$', '{', 'regsubst', 'hiera_hash', 'pick', 'get_ssl_property',
+                             'inline_template', 'under', 'mysql_password', '/', 'ssl_ciphersuite', '::']
               tokens.each do |indi_token|
                   nxt_token     = indi_token.next_code_token # next token which is not a white space
                   if (!nxt_token.nil?) && (!indi_token.nil?)
                      token_type   = indi_token.type.to_s ### this gives type for current token
-   
+
                      token_line   = indi_token.line ### this gives type for current token
                      nxt_tok_line = nxt_token.line
-   
+
                      nxt_nxt_token =  nxt_token.next_code_token # get the next next token to get key value pair
-   
+
                      if  (!nxt_nxt_token.nil?)
                         nxt_nxt_line = nxt_nxt_token.line
                         if (token_type.eql? 'NAME') || (token_type.eql? 'VARIABLE')
@@ -427,11 +431,11 @@ PuppetLint.new_check(:no_hardcode_secret_key) do
                               token_valu   = indi_token.value.downcase
                               nxt_nxt_val  = nxt_nxt_token.value.downcase
                               nxt_nxt_type = nxt_nxt_token.type.to_s  ## to handle false positives,
-                              if (((token_valu.include? "key") || (token_valu.include? "crypt") ||
+                              if ((((token_valu.include? "key") || (token_valu.include? "crypt") ||
                                    (token_valu.include? "secret") || (token_valu.include? "certificate") ||
                                    (token_valu.include? "cert") || (token_valu.include? "token") || (token_valu.include? "ssh_key") ||
                                    (token_valu.include? "md5") || (token_valu.include? "rsa") || (token_valu.include? "ssl") ||
-                                   (token_valu.include? "dsa") 
+                                   (token_valu.include? "dsa")) &&  (! token_valu.include? "::")
                                   ) && ((! token_valu.include? "provider") && (!nxt_nxt_type.eql? 'VARIABLE') && (!invalid_kw_list.include? nxt_nxt_val) && (nxt_nxt_val.length > 1) )
                                  )
                                     notify :warning, {
